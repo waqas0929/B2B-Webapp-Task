a4551c60db9251932b3f016c096a75b41be06434
waqas0929 <waqasraza0929@gmail.com>
2024-06-23 12:09:48 +0500

    correction in cart routes

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import salesController from '../controllers/salesController.js';
-import authMiddleware from '../middleware/authMiddleware.js';
 import cartController from '../controllers/cartController.js';
 import authenticateJWT from '../middleware/authMiddleware.js';
 
 const cartRouter = express.Router();
 
-cartRouter.post('/addToCart', authMiddleware, cartController.addToCart);
-cartRouter.get('/cart/:userId', authMiddleware, salesController.getCartItems);
+cartRouter.post('/addToCart', authenticateJWT, cartController.addToCart);
+cartRouter.get('/cart/:userId', authenticateJWT, salesController.getCartItems);
 cartRouter.delete("/cart/:userId/:productId", authenticateJWT, cartController.removeFromCart);
 cartRouter.put('/cart/:userId/:productId', authenticateJWT, cartController.updateCartItem);
 
